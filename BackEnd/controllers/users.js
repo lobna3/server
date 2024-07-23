9ebbaaa082ca90b966542687d86bd96e904d8bdb
Alafajraoui <fullstackjs110299@gmail.com>
2024-07-23 16:51:34 +0200

    Users modele

--- BackEnd/controllers/users.js
+++ BackEnd/controllers/users.js
@@ -2,9 +2,14 @@ const prisma = require('../database/prisma.js')
 
 
 
+const fetchUsers = async (req, res) => {
+    const users = await prisma.user.findMany({
+    });
 
+    return res.json({ status: 200, data: users });
+};
 
 
 module.exports = {
-    
+    fetchUsers
 }
--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const app = express()
 const cors = require ('cors')
+const usersRoutes = require ('./routes/user.js')
+
 
 
 // * Middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
+app.use('/api/users',usersRoutes)
 
 
 const port = 5000
