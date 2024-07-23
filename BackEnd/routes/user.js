186dcfea8d2289295676e208cbfd14e91ec05be6
Alafajraoui <fullstackjs110299@gmail.com>
2024-07-23 18:23:17 +0200

    Register with validation done in backend

--- BackEnd/routes/user.js
+++ BackEnd/routes/user.js
@@ -1,8 +1,9 @@
-const express = require('express')
-const router = express.Router()
-const {fetchUsers}= require ("../controllers/users")
+const express = require('express');
+const router = express.Router();
+const { fetchUsers } = require("../controllers/users");
+const { validateRegister, registerUser } = require("../controllers/Authentication");
 
+router.get('/getAll', fetchUsers);
+router.post('/register', validateRegister(), registerUser);
 
-router.get('/getAll',fetchUsers)
-
-module.exports= router
+module.exports = router;
